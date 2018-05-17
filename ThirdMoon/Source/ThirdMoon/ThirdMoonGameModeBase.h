27b2cdc4c5cac51f857f6a2b6e2e229159f3111b
Nish Games <rikaendo@alum.calarts.edu>
2018-05-17 14:42:49 -0700

    created ue4 project, game mode, player controller, and player blueprints. imported planet skel mesh fbx

---
+++ ThirdMoon/Source/ThirdMoon/ThirdMoonGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThirdMoonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDMOON_API AThirdMoonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
